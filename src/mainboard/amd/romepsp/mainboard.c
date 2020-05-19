7c4c8ce2a67c50034a4601e7c8900e2887a3966e
Ronald G. Minnich <rminnich@google.com>
2020-05-19 07:48:32 -0700

    checkpoint commit, we are failing in cbfs_boot_map_leak.
    Change-Id: I259a4a481e3808c0807cc1b302858c8bb4bc93c6
    Signed-off-by: Ronald G. Minnich <rminnich@google.com>

--- src/mainboard/amd/romepsp/mainboard.c
+++ src/mainboard/amd/romepsp/mainboard.c
@@ -9,39 +9,10 @@
 #include <cpu/x86/msr.h>
 #include <cpu/amd/msr.h>
 
-#define Q35_PAM0            0x90
-
-static const unsigned char qemu_q35_irqs[] = {
-	10, 10, 11, 11,
-	10, 10, 11, 11,
-};
-
 static void qemu_nb_init(struct device *dev)
 {
 	print_func_entry();
-	/* Map memory at 0xc0000 - 0xfffff */
-	int i;
-	uint8_t v = pci_read_config8(dev, Q35_PAM0);
-	v |= 0x30;
-	pci_write_config8(dev, Q35_PAM0, v);
-	pci_write_config8(dev, Q35_PAM0 + 1, 0x33);
-	pci_write_config8(dev, Q35_PAM0 + 2, 0x33);
-	pci_write_config8(dev, Q35_PAM0 + 3, 0x33);
-	pci_write_config8(dev, Q35_PAM0 + 4, 0x33);
-	pci_write_config8(dev, Q35_PAM0 + 5, 0x33);
-	pci_write_config8(dev, Q35_PAM0 + 6, 0x33);
 
-	/* This sneaked in here, because Qemu does not
-	 * emulate a SuperIO chip
-	 */
-	pc_keyboard_init(NO_AUX_DEVICE);
-
-	/* setup IRQ routing for pci slots */
-	for (i = 0; i < 25; i++)
-		pci_assign_irqs(pcidev_on_root(i, 0), qemu_q35_irqs + (i % 4));
-	/* setup IRQ routing southbridge devices */
-	for (i = 25; i < 32; i++)
-		pci_assign_irqs(pcidev_on_root(i, 0), qemu_q35_irqs);
 	print_func_exit();
 }
 
@@ -53,6 +24,7 @@ static void qemu_nb_read_resources(struct device *dev)
 	/* reserve mmconfig */
 	fixed_mem_resource(dev, 2, CONFIG_MMCONF_BASE_ADDRESS >> 10, 0x10000000 >> 10,
 			   IORESOURCE_RESERVE);
+
 	print_func_exit();
 }
 
@@ -70,13 +42,23 @@ static const struct pci_driver nb_driver __pci_driver = {
 	.device = 0x1480,
 };
 
+/* these are in mtrr.h and I'm not ready for that. */
+#define TOP_MEM		0xC001001Aul
+#define TOP_MEM2	0xC001001Dul
+
+#define TOP_MEM_MASK			0x007fffff
+#define TOP_MEM_MASK_KB			(TOP_MEM_MASK >> 10)
+
+
 static void mainboard_amd_romepsp_enable(struct device *dev)
 {
 	msr_t msr;
+	unsigned long tomk = 0; //, high = 0;
+
 
 	print_func_entry();
 	printk(BIOS_INFO, "Mainboard " CONFIG_MAINBOARD_PART_NUMBER " Enable.\n");
-	db();
+
 	msr = rdmsr(MMIO_CONF_BASE);
 	printk(BIOS_ERR, "c0010058 val %x:%x\n", msr.hi, msr.lo);
 	printk(BIOS_ERR, "Setting c0010058 to %x\n", msr.lo | 0xf8000001);
@@ -84,6 +66,17 @@ static void mainboard_amd_romepsp_enable(struct device *dev)
 	wrmsr(MMIO_CONF_BASE, msr);
 	msr = rdmsr(MMIO_CONF_BASE);
 	printk(BIOS_ERR, "c0010058 val %x:%x\n", msr.hi, msr.lo);
+
+	msr = rdmsr(TOP_MEM);
+	printk(BIOS_ERR, "TOP %x:%x\n", msr.hi,msr.lo);
+	tomk = (msr.lo>>10)&TOP_MEM_MASK;
+	printk(BIOS_DEBUG, "QEMU: cmos: %lu MiB RAM below 4G.\n", tomk / 1024);
+	/* Report the memory regions. */
+	ram_resource(dev, 10, 0, 640);
+	ram_resource(dev, 11, 768, tomk - 768);
+//	if (high)
+//		ram_resource(dev, idx++, 4 * 1024 * 1024, high);
+	db();
 	print_func_exit();
 }
 

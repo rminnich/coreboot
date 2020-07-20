ac569ad830c8c50065ab090028b2882e3df8f000
Ronald G Minnich <rminnich@gmail.com>
2020-07-20 10:38:54 -0700

    This is good enough for a checkpoint.
    output:
    
    00000000  00 1b 63 1b 63 1b 63 31  30 32 33 08 88 08 8a 08  |..c.c.c1023.....|
    00000010  0a 08 88 8a 8a 88 08 08  0a 08 08 08 08 08 08 08  |................|
    00000020  08 08 08 0a 88 08 08 8a  0f dd 5f 5d 5d 5d dd 57  |.........._]]].W|
    00000030  d5 5d dd 0a 08 88 08 0a  08 08 08 08 08 08 08 08  |.]..............|
    00000040  08 08 0a 08 08 08 8a 0a  88 0a 08 08 08 88 02 08  |................|
    00000050  88 08 0a 88 0a 08 08 0a  8a 88 08 08 0a 08 08 08  |................|
    00000060  08 08 08 08 08 88 08 0f  dd 5d 5d df 5f dd 5f 5d  |.........]]._._]|
    00000070  5d 5d 08 02 80 08 08 0a  08 88 08 0a 08 08 08 08  |]]..............|
    00000080  88 08 08 88 08 0a 08 08  08 8a 0a 88 0a 08 08 08  |................|
    00000090  88 02 66 20 69 73 20 6e  6f 74 20 30 78 61 61 35  |..f is not 0xaa5|
    000000a0  35 35 35 61 61 20 61 6e  64 20 73 68 6f 75 6c 64  |555aa and should|
    000000b0  20 62 65 0a 34 35 00                              | be.45.|
    000000b7
    
    so ... after the 23, should be the first message, not garbage.
    
    Also, it dies when it calls ice().
    
    We have no stack maybe? except we can't return from any call.
    
    Change-Id: Idc5b27eb68ec29e1fa34adcdae60ebf995e8cc9a
    Signed-off-by: Ronald G Minnich <rminnich@gmail.com>

--- src/mainboard/amd/rome/bootblock.c
+++ src/mainboard/amd/rome/bootblock.c
@@ -7,17 +7,54 @@
 /*
 #include <baseboard/variants.h>
 */
+static uint32_t fuck = 0xaa5555aa;
+
+static inline void postme(const char *s) {
+	for(uint8_t f = *s++; f; f = *s++) {
+	outb(f, 0x3f8);
+		for(int j = 0; j < 100000000; j++)
+			;
+	}
+}
+
+void sio_put32(u32 data);
 
 void bootblock_mainboard_early_init(void)
 {
 	uint8_t *cp = (void *)0xfedc9000;
+	uint8_t x;
+	void ice(void);
+
+	outb('2', 0x3f8);
+		for(int j = 0; j < 100000000; j++)
+			;
+	outb('3', 0x3f8);
+		for(int j = 0; j < 100000000; j++)
+			;
+//		sio_put32((u32)&fuck);
+	if (read32(&fuck) != 0xaa5555aa) postme("fuck is not 0xaa5555aa and should be\n"); else postme("f is 0xaa5555aa and should be\n");
+	write32(&fuck, 0xdeadbeef);
+	if (read32(&fuck) != 0xdeadbeef) postme("fuck is not 0xdeadbeef and should be\n"); else postme("f is 0xdeabeef and should be\n");
+	if (read32(&fuck) == 0xaa5555aa) postme("fuck is 0xaa5555aa and should not be\n"); else postme("f is not 0xaa5555aa and should be\n");
+
+	outb('4', 0x3f8);
+		for(int j = 0; j < 100000000; j++)
+			;
+	outb('5', 0x3f8);
+		for(int j = 0; j < 100000000; j++)
+			;
+	ice();
 	for(uint8_t i = 0xbb; ; i++) {
 		for(int j = 0; j < 100000000; j++)
 			;
 		outb(i, 0x80);
 		outb('0', 0x3f8);
 		write8(cp, '1');
+		x = inb(0x3f8);
+		if (x == 'f')
+			break;
 	}
+	outb('2', 0x3f8);
 	/*
 	size_t num_gpios;
 	const struct soc_amd_gpio *gpios;

651e19a4fc383f04ca3772c9effe2e5a10995de3
Ronald G Minnich <rminnich@gmail.com>
2020-07-20 15:24:32 -0700

    This gets the alternating 01 output with reasonable timing.
    This is extremely fragile. Put out output too soon and ... no serial. Not sure why. Serial in general on this is fragile.
    
    Change-Id: I80e90f77753849c815b80345210d9150f5fa3508
    Signed-off-by: Ronald G Minnich <rminnich@gmail.com>

--- src/mainboard/amd/rome/bootblock.c
+++ src/mainboard/amd/rome/bootblock.c
@@ -11,8 +11,16 @@ static uint32_t fuck = 0xaa5555aa;
 
 static inline void postme(const char *s) {
 	for(uint8_t f = *s++; f; f = *s++) {
-	outb(f, 0x3f8);
-		for(int j = 0; j < 100000000; j++)
+		if (((f>>4)&0xf) < 10) outb(((f>>4)&0xf)+'0', 0x3f8);
+		else outb(((f>>4)&0xf)+'a'-10,0x3f8);
+		for(int j = 0; read32(&j) < 100; j++)
+			outb(f, 0x80);
+		if (((f)&0xf) < 10) outb(((f)&0xf)+'0', 0x3f8);
+		else outb(((f)&0xf)+'a'-10,0x3f8);
+		for(int j = 0; read32(&j) < 100000000; j++)
+			;
+		outb(',', 0x3f8);
+		for(int j = 0; read32(&j) < 100000000; j++)
 			;
 	}
 }
@@ -25,12 +33,17 @@ void bootblock_mainboard_early_init(void)
 	uint8_t x;
 	void ice(void);
 
+	outb(0x32, 0x80);
+	if (0) {
 	outb('2', 0x3f8);
-		for(int j = 0; j < 100000000; j++)
-			;
+		for(int j = 0; j < 100000; j++)
+			inb(0x3f8);
 	outb('3', 0x3f8);
-		for(int j = 0; j < 100000000; j++)
-			;
+		for(int j = 0; j < 100000; j++)
+			inb(0x3f8);
+	}
+		if (0) {
+		postme("fuck");
 //		sio_put32((u32)&fuck);
 	if (read32(&fuck) != 0xaa5555aa) postme("fuck is not 0xaa5555aa and should be\n"); else postme("f is 0xaa5555aa and should be\n");
 	write32(&fuck, 0xdeadbeef);
@@ -44,10 +57,17 @@ void bootblock_mainboard_early_init(void)
 		for(int j = 0; j < 100000000; j++)
 			;
 	ice();
+		}
 	for(uint8_t i = 0xbb; ; i++) {
-		for(int j = 0; j < 100000000; j++)
-			;
+		for(int j = 0; j < 100000; j++)
+			inb(0x3f8);
 		outb(i, 0x80);
+		/*
+		outb('f', 0x83);
+		outb('u', 0x82);
+		outb('c', 0x81);
+		outb('k', 0x80);
+		*/
 		outb('0', 0x3f8);
 		write8(cp, '1');
 		x = inb(0x3f8);
